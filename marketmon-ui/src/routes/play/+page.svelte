62b2b6ee6a948f0d2a6ec37880c926c8f9ef02af
Chris Tague <cstague6401@gmail.com>
2024-04-06 15:20:54 -0500

    stuff

--- marketmon-ui/src/routes/play/+page.svelte
+++ marketmon-ui/src/routes/play/+page.svelte
@@ -5,19 +5,23 @@
     export let data: PageData;
 </script>
 
-{#each Object.entries(data.data) as card}
-    <Card
-        name={card[0]}
-        health={card[1].health}
-        defense={card[1].defense}
-        attack={card[1].attack}
-        growth={card[1].growth}
-        image={'https://img.pokemondb.net/sprites/scarlet-violet/normal/charizard.png'}
-    />
-{/each}
+<div class='card-container'>
+    {#each Object.entries(data.data) as card}
+        <Card
+            name={card[0]}
+            health={card[1].health}
+            defense={card[1].defense}
+            attack={card[1].attack}
+            growth={card[1].growth}
+            image={'https://img.pokemondb.net/sprites/scarlet-violet/normal/charizard.png'}
+        />
+    {/each}
+</div>
 
-<button on:click={() => {
-    console.log(Object.entries(data.data)[0])
-}}>
-    Click me
-</button>
+<style>
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+</style>
